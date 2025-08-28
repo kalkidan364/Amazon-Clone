d5883e80db6dd3fdd7a0ad518f6788c5316dbef8
kalkidan364 <mengistukalkidan16@gmail.com>
2025-08-27 21:23:02 -0700

    Carousel

--- amazon-clone/src/App.js
+++ amazon-clone/src/App.js
@@ -1,10 +1,12 @@
 
 import './App.css';
 import Header from './components/Header/Header';
+import CarouselEffect from './components/Carousel/Carousel';
 function App() {
   return (
     <div >
    <Header/>
+   <CarouselEffect/>
     </div>
   );
 }
---
+++ amazon-clone/src/components/Carousel/img/data.js
@@ -0,0 +1,6 @@
+import img1 from './amazonimg1.jpg'
+import img2 from "./amazonimg2.jpg";
+import img3 from "./amazonimg3.jpg";
+import img4 from "./amazonimg4.jpg";
+import img5 from "./amazonimg5.jpg";
+export const img=[img1,img2,img3,img4,img5]
