bdc309aa73bdc406d57c763714c0069ace0f163e
kalkidan364 <mengistukalkidan16@gmail.com>
2025-08-30 17:25:20 -0700

    DetailPageRouting

--- amazon-clone/src/components/Product/ProductCard.js
+++ amazon-clone/src/components/Product/ProductCard.js
@@ -1,24 +1,26 @@
 import React from "react";
 import Rating from "@mui/material/Rating";
-import currencyFormat from "../CurrencyFormat/CurrencyFormat";
+import CurrencyFormat from "../CurrencyFormat/CurrencyFormat";
 import classes from "./Product.module.css";
+import { Link } from "react-router-dom";
 const ProductCard = ({ Product }) => {
-  const { image, title, id, rating, price } = Product;
+  if (!Product) return null;
+  const { image, title, id, rating={}, price } = Product;
   return (
     <div className={`${classes.card__container}`}>
-      <a href="">
+      <Link to={`/products/${id}`}>
         <img src={image} alt=" " />
-      </a>
+      </Link>
       <div>
         <h3>{title}</h3>
         <div className={classes.rating}>
           {/*rating */}
-          <Rating value={rating.rate} precision={0.1} />
-          <small>{rating.count}</small>
+          <Rating value={rating.rate || 0} precision={0.1} readOnly />
+          <small>{rating.count || 0}</small>
         </div>
         <div>
           {/*price */}
-          <currencyFormat amount={price} />
+          <CurrencyFormat amount={price} />
         </div>
         <button className={classes.button}>add to cart</button>
       </div>
--- amazon-clone/src/pages/ProductDetail/ProductDetail.js
+++ amazon-clone/src/pages/ProductDetail/ProductDetail.js
@@ -1,9 +1,25 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import LayOut from "../../components/LayOut/LayOut";
+import { useParams } from "react-router-dom";
+import axios from "axios";
+import { productUrl } from "../../Api/EndPoints";
+import ProductCard from "../../components/Product/ProductCard";
 const ProductDetail = () => {
+const {productId} = useParams()
+const [product,setproduct]=useState({})
+  useEffect(()=>{
+    axios.get(`${productUrl}/products/${productId}`)
+    .then((res)=>{
+      setproduct(res.data)
+    })
+    .catch((err)=>{
+      console.log(err)
+    })
+  },[])
+
   return (
     <LayOut>
-      <div>product</div>
+      <ProductCard Product={product}/>
     </LayOut>
   );
 };
