8abc5b41e2bfa9f1a724ac5dac0c83c15774d632
kalkidan364 <mengistukalkidan16@gmail.com>
2025-09-28 01:39:54 -0700

    amazon_api

--- amazon-clone/src/Api/axios.js
+++ amazon-clone/src/Api/axios.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "http://127.0.0.1:5001/clone-61fed/us-central1/api",
+  //baseURL: "http://127.0.0.1:5001/clone-61fed/us-central1/api",
+  baseURL: "https://amazon-clone-berb.onrender.com",
 });
 
 export {axiosInstance}
