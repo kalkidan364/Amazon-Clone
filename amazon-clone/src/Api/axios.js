ab2e49da79083e72fd1f682b55a4bdd9765c8269
kalkidan364 <mengistukalkidan16@gmail.com>
2025-09-28 03:59:23 -0700

    amazon-deploy

--- amazon-clone/src/Api/axios.js
+++ amazon-clone/src/Api/axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const axiosInstance = axios.create({
   //baseURL: "http://127.0.0.1:5001/clone-61fed/us-central1/api",
-  baseURL: "https://amazon-clone-berb.onrender.com",
+  baseURL: "https://amazon-backend-whvs.onrender.com",
 });
 
 export {axiosInstance}
